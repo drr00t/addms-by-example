fcf426f815c039a28795f505603126b1e24a7c81
drr00t <adribeiro@gmail.com>
2024-09-29 22:17:20 -0300

    exemplos

---
+++ ms-cml-restful/src/test/java/com/github/drr00t/RestJsonIT.java
@@ -0,0 +1,10 @@
+
+package org.acme.rest.json;
+
+import com.github.drr00t.RestJsonTest;
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+
+@QuarkusIntegrationTest
+public class RestJsonIT extends RestJsonTest {
+}

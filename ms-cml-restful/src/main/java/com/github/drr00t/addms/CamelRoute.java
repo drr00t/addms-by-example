9693147d9db11c113f09c88bee882274e9c3269f
drr00t <adribeiro@gmail.com>
2024-10-12 18:21:56 -0300

    documentacao

--- ms-cml-restful/src/main/java/com/github/drr00t/addms/CamelRoute.java
+++ ms-cml-restful/src/main/java/com/github/drr00t/addms/CamelRoute.java
@@ -2,21 +2,44 @@ package com.github.drr00t.addms;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
 
 public class CamelRoute extends RouteBuilder {
 
     @Override
     public void configure() {
-        restConfiguration()
-                // .component("servlet")
-                // .port(8080)
-                // .host("127.0.0.1")
-                .apiContextPath("api-docs")
-                // .apiContextIdPattern("#name#")
-                .apiProperty("api.title", "Test REST API")
-                .apiProperty("api.version", "v1")
-                .apiProperty("cors", "true")
-                .bindingMode(RestBindingMode.json);
+        // restConfiguration()
+        //         // .component("servlet")
+        //         // .port(8080)
+        //         // .host("127.0.0.1")
+        //         .apiContextPath("api-docs")
+        //         // .apiContextIdPattern("#name#")
+        //         .apiProperty("api.title", "Test REST API")
+        //         .apiProperty("api.version", "v1")
+        //         .apiProperty("cors", "true")
+        //         .bindingMode(RestBindingMode.json);
+        restConfiguration().component("netty-http").bindingMode(RestBindingMode.json)
+            // and output using pretty print
+            .dataFormatProperty("prettyPrint", "true")
+            // setup context path and port number that netty will use
+            .contextPath("/").port(8080)
+            // add OpenApi api-doc out of the box
+            .apiContextPath("/api-doc")
+                .apiProperty("api.title", "User API").apiProperty("api.version", "1.2.3")
+                // and enable CORS
+                .apiProperty("cors", "true");
+
+                        // this user REST service is json only
+        rest("/user").description("User rest service")
+        .consumes("application/json").produces("application/json")
+        .get("/{id}").description("Find user by id").outType(User.class)
+            .param().name("id").type(RestParamType.path).description("The id of the user to get").dataType("int").endParam()
+            .to("bean:userService?method=getUser(${header.id})")
+        .put().description("Updates or create a user").type(User.class)
+            .param().name("body").type(RestParamType.body).description("The user to update or create").endParam()
+            .to("bean:userService?method=updateUser")
+        .get("/findAll").description("Find all users").outType(User[].class)
+            .to("bean:userService?method=listUsers");
 
         rest().path("api")
             // Dash '-' is not allowed by default
--- ms-cml-restful/src/test/java/com/github/drr00t/addms/RestJsonTest.java
+++ ms-cml-restful/src/test/java/com/github/drr00t/addms/RestJsonTest.java
@@ -1,6 +1,8 @@
 
 package com.github.drr00t.addms;
 
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -12,14 +14,40 @@ import static io.restassured.RestAssured.given;
 @QuarkusTest
 public class RestJsonTest {
 
-    @Test
-    public void dashed() {
-
-        /* Assert the initial fruits are there */
-        given()
-                .when().get("/api/dashed")
-                .then()
-                .statusCode(200);
+        @Test
+        public void testList() {
+            given()
+                    .when().get("/fruits")
+                    .then()
+                    .statusCode(200)
+                    .body("$.size()", is(2),
+                            "name", containsInAnyOrder("Apple", "Pineapple"),
+                            "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
+        }
+    
+        @Test
+        public void testAdd() {
+            given()
+                    .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                //     .header("Content-Type", MediaType.APPLICATION_JSON)
+                    .when()
+                    .post("/fruits")
+                    .then()
+                    .statusCode(200)
+                    .body("$.size()", is(3),
+                            "name", containsInAnyOrder("Apple", "Pineapple", "Pear"),
+                            "description", containsInAnyOrder("Winter fruit", "Tropical fruit", "Winter fruit"));
+    
+            given()
+                    .body("{\"name\": \"Pear\", \"description\": \"Winter fruit\"}")
+                //     .header("Content-Type", MediaType.APPLICATION_JSON)
+                    .when()
+                    .delete("/fruits")
+                    .then()
+                    .statusCode(200)
+                    .body("$.size()", is(2),
+                            "name", containsInAnyOrder("Apple", "Pineapple"),
+                            "description", containsInAnyOrder("Winter fruit", "Tropical fruit"));
         }
 
 }
